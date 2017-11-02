35f6e198fb729784f10ec1958cb10dbd70c918f3
gzpeterz <zhangwen@gzpeter.com>
2017-11-02 11:18:44 +0800

    first example is working

---
+++ src/main/java/com/hc/springmvc/controller/ItemController1.java
@@ -0,0 +1,42 @@
+package com.hc.springmvc.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.hc.mybatis.po.Items;
+
+public class ItemController1 implements Controller {
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		List<Items> itemsList = new ArrayList<Items>();
+
+		Items items_1 = new Items();
+		items_1.setName("联想笔记本");
+		items_1.setPrice(6000f);
+		items_1.setCreatetime(new Date());
+		items_1.setDetail("ThinkPad T430 联想笔记本电脑！");
+
+		Items items_2 = new Items();
+		items_2.setName("苹果手机");
+		items_2.setPrice(5000f);
+		items_2.setDetail("iphone6苹果手机！");
+
+		itemsList.add(items_1);
+		itemsList.add(items_2);
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("itemsList", itemsList);
+		modelAndView.addObject("aaaa", "this is from controler");
+		modelAndView.setViewName("/WEB-INF/jsp/itemsList.jsp");
+		return modelAndView;
+	}
+
+}
